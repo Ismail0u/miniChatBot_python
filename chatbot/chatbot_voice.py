326e98e835c9f2811548dc64de993cef9159e283
Ismail0u <moussaisma05@gmail.com>
2025-04-08 14:58:37 +0200

    deuxieme version

--- chatbot/chatbot_voice.py
+++ chatbot/chatbot_voice.py
@@ -1,14 +1,12 @@
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
 import speech_recognition as sr
-import pyttsx3
-import threading
-import time
+import io
 
 class VoiceHelper:
     def __init__(self):
         self.recognizer = sr.Recognizer()
-        self.tts_engine = pyttsx3.init()
-        self.tts_engine.setProperty('rate', 160)  # Vitesse de la voix
-        self.tts_engine.setProperty('volume', 0.9)  # Volume de la voix
 
     def listen(self):
         with sr.Microphone() as source:
@@ -26,13 +24,15 @@ class VoiceHelper:
                 return "Tu n'as rien dit 😅."
 
     def speak(self, text):
-        # Parler dans un thread séparé
-        thread = threading.Thread(target=self._speak_internal, args=(text,))
-        thread.start()
-
-    def _speak_internal(self, text):
         try:
-            self.tts_engine.say(text)
-            self.tts_engine.runAndWait()
+            # Convertir le texte en MP3 dans la mémoire
+            tts = gTTS(text=text, lang='fr')
+            fp = io.BytesIO()
+            tts.write_to_fp(fp)
+            fp.seek(0)
+
+            # Lire le fichier MP3 en mémoire
+            audio = AudioSegment.from_file(fp, format="mp3")
+            play(audio)
         except Exception as e:
             print(f"Erreur dans la lecture vocale : {e}")
